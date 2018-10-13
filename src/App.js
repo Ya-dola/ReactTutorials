e5ec8ba732451cec3f16a93d1219bd69222ebde5
Ya-dola <Ya-dola@users.noreply.github.com>
2018-10-13 22:02:02 +0300

    Set Up Dynamic Person Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Test React Works</h1>
+        <Person name = "Aadil" />
       </div>
     );
   }
