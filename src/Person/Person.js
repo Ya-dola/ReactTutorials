35e1b4b3858d7a611b5254eed599aa697e737c56
Ya-dola <Ya-dola@users.noreply.github.com>
2018-10-16 22:36:14 +0300

    Added Styling to Person

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Person.css";
 
 const person = props => {
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.click}>I am {props.name} !!!</p>
       <p>{props.children}</p>
       <input type="Text" onChange={props.changed} value={props.name} />
