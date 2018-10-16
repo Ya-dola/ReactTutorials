d197530018ca28e9b81cb899f531480edc8e4344
Ya-dola <Ya-dola@users.noreply.github.com>
2018-10-16 20:50:38 +0300

    Added States for Person

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [{ name: "Aadil" }, { name: "Zoy" }]
+  };
+
+  swtichNamesHandler = () => {
+    console.log("Button Clicked !");
+    this.setState({ persons: [{ name: "Dola" }, { name: "Zoy" }] });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Test React Works</h1>
-        <Person name = "Aadil" />
+        <button onClick={this.swtichNamesHandler}>Switch Names</button>
+        <Person name={this.state.persons[0].name} />
+        <Person name={this.state.persons[1].name}>Elloooo</Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const person = props => {
-  return <p>I am {props.name} !!!</p>;
+  return (
+    <div>
+      <p>I am {props.name} !!!</p>
+      <p>{props.children}</p>
+    </div>
+  );
 };
 
 export default person;
